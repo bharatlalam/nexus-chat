75083dbffe94a908e7fbe55672046a9e4a38a2b7
bharatlalam <323103310133@gvpce.ac.in>
2026-07-08 11:15:44 +0530

    fix: dns ipv4first for smtp

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,3 +1,5 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
